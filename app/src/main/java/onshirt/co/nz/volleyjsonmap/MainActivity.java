4a2c78c21d256be8b4decd6cb9145628593bc672
offhui <offhui@hotmail.com>
2019-07-02 23:47:20 +1200

    test

--- app/src/main/java/onshirt/co/nz/volleyjsonmap/MainActivity.java
+++ app/src/main/java/onshirt/co/nz/volleyjsonmap/MainActivity.java
@@ -129,44 +129,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static void removeSimpleProgressDialog() {
-        try {
-            if (mProgressDialog != null) {
-                if (mProgressDialog.isShowing()) {
-                    mProgressDialog.dismiss();
-                    mProgressDialog = null;
-                }
-            }
-        } catch (IllegalArgumentException ie) {
-            ie.printStackTrace();
-
-        } catch (RuntimeException re) {
-            re.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void showSimpleProgressDialog(Context context, String title,
-                                                String msg, boolean isCancelable) {
-        try {
-            if (mProgressDialog == null) {
-                mProgressDialog = ProgressDialog.show(context, title, msg);
-                mProgressDialog.setCancelable(isCancelable);
-            }
-
-            if (!mProgressDialog.isShowing()) {
-                mProgressDialog.show();
-            }
-
-        } catch (IllegalArgumentException ie) {
-            ie.printStackTrace();
-        } catch (RuntimeException re) {
-            re.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void removeSimpleProgressDialog() {
+//        try {
+//            if (mProgressDialog != null) {
+//                if (mProgressDialog.isShowing()) {
+//                    mProgressDialog.dismiss();
+//                    mProgressDialog = null;
+//                }
+//            }
+//        } catch (IllegalArgumentException ie) {
+//            ie.printStackTrace();
+//
+//        } catch (RuntimeException re) {
+//            re.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public static void showSimpleProgressDialog(Context context, String title,
+//                                                String msg, boolean isCancelable) {
+//        try {
+//            if (mProgressDialog == null) {
+//                mProgressDialog = ProgressDialog.show(context, title, msg);
+//                mProgressDialog.setCancelable(isCancelable);
+//            }
+//
+//            if (!mProgressDialog.isShowing()) {
+//                mProgressDialog.show();
+//            }
+//
+//        } catch (IllegalArgumentException ie) {
+//            ie.printStackTrace();
+//        } catch (RuntimeException re) {
+//            re.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
